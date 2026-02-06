1a658c64827673a93bf6119d81359b00fa3fa517
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-02-05 22:31:01 -0600

    Cache product results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
   def index
     StatsD.measure("products.index.time") do
-      render json: { products: Product.to_json }
+      products_json = Rails.cache.fetch("products", expires_in: 2.minutes) do
+        Product.to_json
+      end
+
+      render json: { products: products_json }
     end
   end
 end
